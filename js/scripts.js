03cf4276aa9006bd9bc95883a0e207f10e5d5014
Jessica Sheridan and Shyamal Punekar <jessica+shyamal@>
2017-07-05 11:20:56 -0700

    include addition flashcard entries. Known issue with CSS selector for ul in 'variable-naming-conventions'.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,32 @@ $(document).ready(function() {
         $("#operator .defbox").toggle();
   });
 
+  $("#Variables").click(function() {
+        $("#Variables .term").toggle();
+        $("#Variables .defbox").toggle();
+  });
 
+  $("#Variable-naming-conventions").click(function() {
+        $("#Variable-naming-conventions .term").toggle();
+        $("#Variable-naming-conventions ul.defbox > li").toggle();
+  });
 
+  $("#Functions").click(function() {
+        $("#Functions .term").toggle();
+        $("#Functions .defbox").toggle();
+  });
+
+  $("#Methods").click(function() {
+        $("#Methods .term").toggle();
+        $("#Methods .defbox").toggle();
+  });
+
+  $("#Arguments").click(function() {
+        $("#Arguments .term").toggle();
+        $("#Arguments .defbox").toggle();
+  });
+  $("#Parameters").click(function() {
+        $("#Parameters .term").toggle();
+        $("#Parameters .defbox").toggle();
+  });
 });
